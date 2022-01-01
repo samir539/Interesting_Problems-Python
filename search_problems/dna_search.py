00353ae3b5a38229ebd84a3197cf2cf6b55ec9a3
samir539 <sc9018@ic.ac.uk>
2022-01-01 20:32:02 +0000

    codon representation

--- search_problems/dna_search.py
+++ search_problems/dna_search.py
@@ -5,3 +5,7 @@ from typing import List, Tuple
 #Nucleotide bases represented using IntEnum
 nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 print(repr(nucleotide.A))
+
+#Representing a Codon
+Codon = Tuple[nucleotide, nucleotide, nucleotide]
+Gene = List[Codon]
