0157649bd0a7277cf18c2bdaba4df3a8dd4c73c0
samir539 <sc9018@ic.ac.uk>
2022-01-02 20:03:37 +0000

    to_gene function

--- search_problems/dna_search.py
+++ search_problems/dna_search.py
@@ -6,6 +6,24 @@ from typing import List, Tuple
 nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 print(repr(nucleotide.A))
 
-#Representing a Codon
+#Representing a Codon and Gene TYPING ALIAS
 Codon = Tuple[nucleotide, nucleotide, nucleotide]
 Gene = List[Codon]
+
+print(type(Gene))
+## Input of gene is a string of letters
+example_Gene: str = 'ACCCGGC'
+# print(example_Gene)
+
+def to_gene(input_gene:str) -> Gene:
+    '''
+    Take input gene in string format and outputs the gene as a variable of the 'Gene' type
+    
+    Args:
+        input_gene (str): the input gene in a string representation
+    
+    Returns:
+        gene (Gene): A list of codons which make up the gene represented in the string 
+    '''
+for i in example_Gene:
+    gene: Gene = []
