b3ed9b457d23f2c1705d2cee3e4842a989c4fcb1
samir539 <sc9018@ic.ac.uk>
2022-01-11 20:03:36 +0000

    more imports

--- search_problems/DNA Search/general_search.py
+++ search_problems/DNA Search/general_search.py
@@ -1,2 +1,9 @@
 from __future__ import annotations
 from typing import TypeVar, Iterable, Sequence, Generic, List, Callable, Set, Deque, Dict, Any, Optional
+from typing_extensions import Protocol
+from heapq import heappush, heappop
+
+T = TypeVar('T')
+
+def linear_contains():
+    pass
