fcabe355e4e1283665b52083ff8e36d310b57470
samir539 <sc9018@ic.ac.uk>
2022-01-15 19:54:54 +0000

    cell class

--- search_problems/maze_solve.py
+++ search_problems/maze_solve.py
@@ -5,3 +5,7 @@ from math import sqrt
 
 #We have a maze of cells, empty cell is " ", occupied cell is "X"
 #let the types of elements on the maze be with an enum
+
+class Cell(str, Enum):
+    empty = " "
+    blocked = "X"
